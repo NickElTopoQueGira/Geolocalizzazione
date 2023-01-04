8adbd994ecc71b4454bd71804f545735f5fd3be1
NickElTopoQueGira <niccolosesana@outlook.it>
2023-01-04 20:05:32 +0100

    abbellimento del codice
    migliorata la modularità dei metodi

--- Geolocalizzazione/MainPage.xaml.cs
+++ Geolocalizzazione/MainPage.xaml.cs
@@ -1,40 +1,56 @@
 ﻿using System.Diagnostics;
 using System.Reflection.Metadata;
 using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.VisualBasic;
 
 namespace Geolocalizzazione;
 
 public partial class MainPage : ContentPage{
 
-	public MainPage(){
+	private PermissionStatus permessi = PermissionStatus.Unknown; 
+
+
+    public MainPage(){
 		InitializeComponent();
+		VerificaPermessi();
 	}
-	
+
+	protected async void VerificaPermessi()
+	{
+        // Controllo dei permessi
+        permessi = await Permissions.CheckStatusAsync < Permissions.LocationWhenInUse>();
+
+        if (permessi != PermissionStatus.Granted)
+		{
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+            {
+                await App.Current.MainPage.DisplayAlert("Richiesti i permessi di geolocalizzazione", "Permessi di geolocalizzazione necessari", "OK");
+                permessi = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            }
+            else if (DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                await App.Current.MainPage.DisplayAlert("Richiesti i permessi di geolocalizzazione", "Permessi di geolocalizzazione necessari", "OK");
+                permessi = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            }
+            else
+            {
+                await App.Current.MainPage.DisplayAlert("Dispositivo non supportato", "Dispositivo non supportato", "OK");
+            }
+        }
+    }
+
     private async void LocalizzazioneBtn_Clicked(object sender, EventArgs e){
-		// Controllo dei permessi
-		var permessi = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
 
 		// se i permessi sono garantiti, viene chiamato il metodo per la geolocalizzazione
-		if(permessi == PermissionStatus.Granted){
+		if(permessi == PermissionStatus.Granted)
+        {
 			await Geolocalizza();
-		}else{
-			await App.Current.MainPage.DisplayAlert("Diritti non presenti", "Premere su OK per fornire i permessi di localizzazione", "OK");
-
-			var richiesti = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
-
-			if (richiesti == PermissionStatus.Granted){
-				await Geolocalizza();
-
-			}else{
-				if (DeviceInfo.Platform == DevicePlatform.Android){
-					await App.Current.MainPage.DisplayAlert("Richiesti i permessi di geolocalizzazione", "Permessi di geolocalizzazione necessari", "OK");
-				}else if (DeviceInfo.Platform == DevicePlatform.iOS){
-					await App.Current.MainPage.DisplayAlert("Richiesti i permessi di geolocalizzazione", "Permessi di geolocalizzazione necessari", "OK");
-				}else{
-					await App.Current.MainPage.DisplayAlert("Dispositivo non supportato", "Dispositivo non supportato", "OK");
-				}
-			}
 		}
+        else
+        {
+            await App.Current.MainPage.DisplayAlert("Richiesti i permessi di geolocalizzazione", "Permessi di geolocalizzazione necessari", "OK");
+            VerificaPermessi();
+        }
 
     }
 
@@ -55,32 +71,19 @@ public partial class MainPage : ContentPage{
 		SalvaDati(text);
 	}
 
-	private void SalvaDati(string text){
+	private void SalvaDati(string text)
+    {
         SavePosition salvaPosizioni = new();
 
-		var path = "";
+		string path = salvaPosizioni.PathDispositivo();
 
-		if(DeviceInfo.Platform == DevicePlatform.Android){
-            // per l'accesso da parte dell'utente alla cartella appdata dell'applicazione che si trova sotto
-            // /data/user/0/com.nick.geolocalizzazione/files/
-            // sono richiesti i privilegi di root
-
-            // andorid document folder
-            path = @"/sdcard/Documents/position.txt";
-		}else if(DeviceInfo.Platform == DevicePlatform.iOS){
-			path = Path.Combine(
-				FileSystem.Current.AppDataDirectory, 
-				"position.txt");
-		}
-
-		// Visualizzazione sulla label
-		PathLabel.Text = path;
+        // Visualizzazione sulla label
+        PathLabel.Text = path;
 
 		// slavataggio della stringa in un file
-		if(salvaPosizioni.verificaEsistenza(path)){
-            salvaPosizioni.addLine(path, text);
-		}else{
-			salvaPosizioni.creaFile(path);
-		}
+		if (salvaPosizioni.VerificaEsistenza(path))
+			salvaPosizioni.AddLine(path, text);
+		else if (!salvaPosizioni.VerificaEsistenza(path))
+			salvaPosizioni.CreaFile(path, text);
     }
 }
---
+++ Geolocalizzazione/SaveData/SavePosition.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * !INFO!
+ * 
+ * Questa classe, si occupa del salvataggio offline dei dati
+ * i dati vengono salvati in un file sotta la cartella documenti, se si tratta di un dispositivo Andoid
+ * se si usa un dipositivo iOS, i dati vengono salvati nella cartella appdata dell'app
+ *
+ */
+
+namespace Geolocalizzazione
+{
+    public class SavePosition
+    {
+        public string PathDispositivo()
+        {
+            string path;
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+            {
+                // per l'accesso da parte dell'utente alla cartella appdata dell'applicazione che si trova sotto
+                // /data/user/0/com.nick.geolocalizzazione/files/
+                // sono richiesti i privilegi di root
+
+                // andorid document folder
+                path = @"/sdcard/Documents/position.txt";
+            }
+            else if (DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                path = Path.Combine(
+                    FileSystem.Current.AppDataDirectory,
+                    "position.txt");
+            }
+            else
+            {
+                path = null;
+            }
+
+            return path;
+        }
+
+        public bool VerificaEsistenza(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception e)
+            {
+                App.Current.MainPage.DisplayAlert("Errore", e.ToString(), "OK");
+                return false;
+            }
+        }
+
+        public void CreaFile(string path,string text)
+        {
+            try
+            {
+                File.Create(path).Close();
+                AddLine(path, text);
+            }
+            catch(Exception e)
+            {
+                App.Current.MainPage.DisplayAlert("Errore", e.ToString() , "OK");
+            }
+            
+        }
+
+        public void AddLine(string path, string text)
+        {            
+            try
+            {
+                StreamWriter sw = new (path, true);
+                sw.WriteLine(text);
+                sw.Close();
+
+            }
+            catch (Exception e)
+            {
+                App.Current.MainPage.DisplayAlert("Errore", e.ToString(), "OK");
+            }
+        }
+    }
+}

a9d4bf3a4313a67a2f7f2ce7d0ffd793ad1cf5ed
NickElTopoQueGira <niccolosesana@outlook.it>
2023-01-04 16:24:43 +0100

    Salvataggio differenziato per piattaforma
    Aggiunta un sistema di salvataggio differenziato per il tipo di SO utilizzato.

--- Geolocalizzazione/MainPage.xaml.cs
+++ Geolocalizzazione/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using static System.Net.Mime.MediaTypeNames;
+﻿using System.Diagnostics;
+using System.Reflection.Metadata;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 
 namespace Geolocalizzazione;
 
@@ -14,14 +16,14 @@ public partial class MainPage : ContentPage{
 
 		// se i permessi sono garantiti, viene chiamato il metodo per la geolocalizzazione
 		if(permessi == PermissionStatus.Granted){
-			await geolocalizza();
+			await Geolocalizza();
 		}else{
 			await App.Current.MainPage.DisplayAlert("Diritti non presenti", "Premere su OK per fornire i permessi di localizzazione", "OK");
 
 			var richiesti = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
 
 			if (richiesti == PermissionStatus.Granted){
-				await geolocalizza();
+				await Geolocalizza();
 
 			}else{
 				if (DeviceInfo.Platform == DevicePlatform.Android){
@@ -36,35 +38,46 @@ public partial class MainPage : ContentPage{
 
     }
 
-
-	private async Task geolocalizza(){
+	private async Task Geolocalizza(){
 		var richiesta = new GeolocationRequest(GeolocationAccuracy.Best);
 		var datiGeolocalizzazione = await Geolocation.GetLocationAsync(richiesta);
 
-		// Notifica su schermo
-		//await App.Current.MainPage.DisplayAlert("GEOLOCALIZZATO",
-		//	$"Latitudine: {datiGeolocalizzazione.Latitude} - Longitudine: {datiGeolocalizzazione.Longitude} - Altitudine: {datiGeolocalizzazione.Altitude}" , "OK");
-		
-		// Visualizzazione sulla label
-		Visualizza.Text = datiGeolocalizzazione.ToString();
+        string text = $"Latitudine: {datiGeolocalizzazione.Latitude} - Longitudine: {datiGeolocalizzazione.Longitude} - Altitudine: {datiGeolocalizzazione.Altitude}";
+
+        // Notifica su schermo
+        //await App.Current.MainPage.DisplayAlert("GEOLOCALIZZATO",
+        //	text , "OK");
+
+        // Visualizzazione sulla label
+        Visualizza.Text = text;
 
 		// slavataggio dei dati in un file .csv
-		
-		//SalvaDati(
-		//	$"Latitudine: {datiGeolocalizzazione.Latitude} - " +
-		//	$"Longitudine: {datiGeolocalizzazione.Longitude} - " +
-		//	$"Altitudine: {datiGeolocalizzazione.Altitude} ");
-		
-		SalvaDati(datiGeolocalizzazione.ToString());
+		SalvaDati(text);
 	}
 
 	private void SalvaDati(string text){
-        SavePosition salvaPosizioni = new SavePosition();
+        SavePosition salvaPosizioni = new();
 
-		var generalPath = FileSystem.Current.AppDataDirectory;
-		var path = Path.Combine(generalPath, "position.txt");
+		var path = "";
 
-		if (salvaPosizioni.verificaEsistenza(path)){
+		if(DeviceInfo.Platform == DevicePlatform.Android){
+            // per l'accesso da parte dell'utente alla cartella appdata dell'applicazione che si trova sotto
+            // /data/user/0/com.nick.geolocalizzazione/files/
+            // sono richiesti i privilegi di root
+
+            // andorid document folder
+            path = @"/sdcard/Documents/position.txt";
+		}else if(DeviceInfo.Platform == DevicePlatform.iOS){
+			path = Path.Combine(
+				FileSystem.Current.AppDataDirectory, 
+				"position.txt");
+		}
+
+		// Visualizzazione sulla label
+		PathLabel.Text = path;
+
+		// slavataggio della stringa in un file
+		if(salvaPosizioni.verificaEsistenza(path)){
             salvaPosizioni.addLine(path, text);
 		}else{
 			salvaPosizioni.creaFile(path);
